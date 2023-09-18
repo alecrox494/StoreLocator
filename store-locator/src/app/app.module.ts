ba33ee327358218f6e15aab8f152cd9cf89cfc66
alecrox494 <alec494.st@gmail.com>
2023-09-18 12:40:22 +0200

    generate components: stores-list, store-details; services: stores data

--- store-locator/src/app/app.module.ts
+++ store-locator/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoresListComponent } from './components/stores-list/stores-list.component';
+import { StoreDetailsComponent } from './components/store-details/store-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoresListComponent,
+    StoreDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ store-locator/src/app/components/stores-list/stores-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stores-list',
+  templateUrl: './stores-list.component.html',
+  styleUrls: ['./stores-list.component.scss']
+})
+export class StoresListComponent {
+
+}
