a88aa4490b9e710932ad08b46b62b4b551f41282
alecrox494 <alec494.st@gmail.com>
2023-09-20 13:25:19 +0200

    select a store and show store-details.component

--- store-locator/src/app/components/store-details/store-details.component.ts
+++ store-locator/src/app/components/store-details/store-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from 'src/app/interfaces/store.interface';
+import { StoresDataService } from 'src/app/services/stores-data.service';
 
 @Component({
   selector: 'app-store-details',
   templateUrl: './store-details.component.html',
   styleUrls: ['./store-details.component.scss']
 })
-export class StoreDetailsComponent {
+export class StoreDetailsComponent implements OnInit {
+  @Input() storeIDs!: Observable<number>;
+  store: Store = {} as Store;
+
+  constructor(private storeService: StoresDataService) { }
+
+  ngOnInit(): void {
+    this.storeIDs.subscribe((id) => {this.store = this.storeService.getStoreById(id) as Store;});
+  }
 
 }
--- store-locator/src/app/components/stores-list/stores-list.component.ts
+++ store-locator/src/app/components/stores-list/stores-list.component.ts
@@ -1,4 +1,5 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit, ElementRef} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Store } from 'src/app/interfaces/store.interface';
 import { StoresDataService } from 'src/app/services/stores-data.service';
 
@@ -11,11 +12,33 @@ export class StoresListComponent implements OnInit {
   displayedColumns: string[] = ['ID', 'address', 'bookable', 'countryIso', 'email', 'hasPickupInStore', 'hasTailorBooking', 'lat', 'lng'];
   storeList: Array<Store> = [];
   clickedRows = new Set<Store>();
+  storeIDs:BehaviorSubject <number> = new BehaviorSubject<number>(0);
+  storeClicked: boolean = false;
 
-  constructor(private storeService: StoresDataService) { }
+  constructor(private storeService: StoresDataService, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     this.storeList = this.storeService.getStoresList();
+    this.storeIDs.subscribe((id) => {
+      console.log(id);
+    });
+  }
+
+  clickRow(row: Store, event: Event) {
+    const target = event.target as HTMLTableCellElement;
+    const container = target.parentElement as HTMLTableRowElement;
+    this.addActiveRow(container);
+    const storeData = row;
+    this.storeIDs.next(storeData.ID);
+    this.storeClicked = true;
+  }
+
+  addActiveRow(element: HTMLTableRowElement) {
+    const lastActive = this.elementRef.nativeElement.querySelector('.store-row-is-clicked');
+    if (lastActive) {
+      lastActive.classList.remove('store-row-is-clicked');
+    }
+    element.classList.add('store-row-is-clicked');
   }
 
 }
