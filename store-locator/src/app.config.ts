cbc1f5b6b78b20b0412781feb036750b4fdd166a
alecrox494 <alec494.st@gmail.com>
2023-09-25 18:09:10 +0200

    Remove GMAP_APY_KEY and add secret configuration file

--- store-locator/src/app.config.ts
+++ store-locator/src/app.config.ts
@@ -1,5 +1,7 @@
 export class AppConfig {
-  private static GMAP_APY_KEY: string  = 'AIzaSyD-C4lolhAKwX2ncmDjW4O44zE6npETbS8';;
+  //this file is a copy of app.config.secret.ts without data
+  //to use it you have to searching for the import path "./app.config.secret" and replace it with "./app.config"
+  private static GMAP_APY_KEY: string  = ''; //the old key was disabled and removed from Google's cloud console
 
   public static getGMapApiKey(): string {
     return this.GMAP_APY_KEY;
