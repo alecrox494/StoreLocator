dc0e61e9462f5ca1d7db10b41cf841b2a92a7c76
alecrox494 <alec494.st@gmail.com>
2023-10-10 12:02:37 +0200

    maps service manage multicoords markers

--- store-locator/src/app/services/google-map.service.ts
+++ store-locator/src/app/services/google-map.service.ts
@@ -7,60 +7,92 @@ import { MapCoords } from '../interfaces/map.interface';
   providedIn: 'root'
 })
 export class GoogleMapService {
+  ZOOM: number = 10;
 
   constructor() { }
 
-  //https://stackoverflow.com/questions/10268033/google-maps-api-v3-method-fitbounds
-
   async initMap( coords: MapCoords[], mapElement:HTMLElement, opts?: AppMapOptions | undefined ): Promise<void> {
     // @ts-ignore
-    const { Map} = await google.maps.importLibrary("maps") as google.maps.MapsLibrary,
+    const { Map} = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
     // @ts-ignore
-    const { Marker } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+    let position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined,
     // @ts-ignore
-    let position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined;
+    bounds = new google.maps.LatLngBounds();
+    // @ts-ignore
+    let mapOptions: google.maps.MapOptions;
+
 
     const isMultiCoords: boolean = coords.length > 1;
 
     // Prepare coordinates
     if (isMultiCoords) {
       //multicoords
+      for (let i = 0; i < coords.length; i++) {
+        // @ts-ignore
+        bounds.extend(new google.maps.LatLng(coords[i].lat, coords[i].lng))
+      }
+      mapOptions = this.manageMultiCoordsMapOptions(opts);
     } else {
       // single coords
       position = coords[0];
+      mapOptions = this.manageSingleCoordMapOptions(opts, position);
     }
-
-    // The map options
-    const mapOptions = this.manageMapOptions(opts, position);
 
     // The map
     const map = new Map(mapElement,  mapOptions);
 
-    // The marker
-    const marker = new Marker({
+     // The marker
+    if (isMultiCoords) {
+      map.fitBounds(bounds);
+      // @ts-ignore
+      map.panToBounds(bounds);
+      coords.forEach(coord => {
+        this.setMarker(coord, map);
+      });
+    } else {
+      this.setMarker(coords[0], map);
+    }
+
+  }
+
+   // @ts-ignore
+  setMarker(coords: MapCoords, map: google.maps.Map, placeName:string = 'Place Name'): void {
+     // @ts-ignore
+    const marker = new google.maps.Marker({
       map: map,
-      position: position,
-      title: 'Place Name'
+      position: coords,
+      title: placeName
     });
   }
 
   // @ts-ignore
-  manageMapOptions(opts: AppMapOptions | undefined, position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined): google.maps.MapOptions {
-    // @ts-ignore
-    let mapOptions: google.maps.MapOptions,
-    zoom = 10,
-    mapId = Math.random().toString(6);
-
+  manageSingleCoordMapOptions(opts: AppMapOptions | undefined, position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined): google.maps.MapOptions {
     if (opts) {
-      zoom = opts.zoom ? opts.zoom : 10;
-      mapId = opts.mapID ? opts.mapID : Math.random().toString(6);
+      this.ZOOM = this.setZoom(opts.zoom);
     }
 
-    return mapOptions = {
-      zoom: zoom,
+    return {
+      zoom: this.ZOOM,
       center: position,
       disableDefaultUI: true,
       styles: AppConfig.getGMapStyle(),
     }
+  }
+
+  // @ts-ignore
+  manageMultiCoordsMapOptions(opts: AppMapOptions | undefined): google.maps.MapOptions {
+    if (opts) {
+      this.ZOOM = this.setZoom(opts.zoom);
+    }
+
+    return {
+      zoom: this.ZOOM,
+      disableDefaultUI: true,
+      styles: AppConfig.getGMapStyle(),
+    }
+  }
+
+  setZoom(zoom: number | undefined): number {
+    return zoom ? zoom : 10;
   }
 }
